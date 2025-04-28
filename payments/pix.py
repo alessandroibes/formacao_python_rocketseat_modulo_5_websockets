589e9fd987b9d24ac34ee4ffcefd42fd306f80a2
alessandroibes <alessandro.ibes@gmail.com>
2025-04-27 21:08:31 -0300

    Add method to connect with Pix partner and generate a QR code

---
+++ payments/pix.py
@@ -0,0 +1,25 @@
+import qrcode
+import uuid
+
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        # criar pagamento na instituição financeira
+        bank_payment_id = uuid.uuid4()
+        
+        # código copia e cola
+        hash_payment = f"hash_payment_{bank_payment_id}"
+
+        # qr_code
+        img = qrcode.make(hash_payment)
+
+        # salvar imagem como arquivo PNG
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}"
+        }
