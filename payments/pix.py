89f52d40f3e6d68235cd6f272307224e5570c8e2
alessandroibes <alessandro.ibes@gmail.com>
2025-04-28 16:07:12 -0300

    Add unit test fot the create_payment method

--- payments/pix.py
+++ payments/pix.py
@@ -6,7 +6,7 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # criar pagamento na instituição financeira
         bank_payment_id = str(uuid.uuid4())
         
@@ -17,7 +17,7 @@ class Pix:
         img = qrcode.make(hash_payment)
 
         # salvar imagem como arquivo PNG
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
